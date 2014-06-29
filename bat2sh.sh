cc3bec14db2fb36f9af5560101f36d2b507cd0a4
meshr-net <dev@meshr.net>
2014-06-29 18:44:23 +0400

    Update bat2sh.sh

--- bat2sh.sh
+++ bat2sh.sh
@@ -1,9 +1,10 @@
 #!/bin/sh
-sed -i "s/%\([^%]\+\)%/\$\1/g" *.bat #Variable conversion
-sed -i "s/if not defined \([^ ]\+\) /[ -z \"\$\1\" ] \&\& /gI" *.bat
-sed -i "s/if defined \([^ ]\+\) /[ -n \"\$\1\" ] \&\& /gI" *.bat
-sed -i "s/if not exist \([^ ]\+\) /[ ! -f \$\1 ] \&\& /gI" *.bat
-sed -i "s/if exist \([^ ]\+\) /[ -f \$\1 ] \&\& /gI" *.bat
-sed -i "s/\^|/|/gI" *.bat  #path conversion
-sed -i "s/^rem /# /gI" *.bat
-sed -i "s/ rem /# /gI" *.bat
+files="*.bat"
+sed -i "s/%\([^%]\+\)%/\$\1/g" $files #Variable conversion
+sed -i "s/if not defined \([^ ]\+\) /[ -z \"\$\1\" ] \&\& /gI" $files
+sed -i "s/if defined \([^ ]\+\) /[ -n \"\$\1\" ] \&\& /gI" $files
+sed -i "s/if not exist \([^ ]\+\) /[ ! -f \$\1 ] \&\& /gI" $files
+sed -i "s/if exist \([^ ]\+\) /[ -f \$\1 ] \&\& /gI" $files
+sed -i "s/\^|/|/gI" $files  #path conversion
+sed -i "s/^rem /# /gI" $files
+sed -i "s/ rem /# /gI" $files
