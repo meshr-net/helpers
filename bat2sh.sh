e4235c7ced69274c42e7d3e82335d0832c15565b
meshr-net <dev@meshr.net>
2014-06-30 01:51:25 +0400

    Update bat2sh.sh

--- bat2sh.sh
+++ bat2sh.sh
@@ -1,11 +1,17 @@
 #!/bin/sh
-sed -i "s/%\([^%]\+\)%/\$\1/g" *.bat #Variable conversion
-sed -i "s/if not defined \([^ ]\+\) /[ -z \"\$\1\" ] \&\& /gI" *.bat
-sed -i "s/if defined \([^ ]\+\) /[ -n \"\$\1\" ] \&\& /gI" *.bat
-sed -i "s/if not exist \([^ ]\+\) /[ ! -f \$\1 ] \&\& /gI" *.bat
-sed -i "s/if exist \([^ ]\+\) /[ -f \$\1 ] \&\& /gI" *.bat
-sed -i "s/\\\\/\//gI" *.bat  #path conversion
-sed -i "s/\^|/|/gI" *.bat #backslashed pipe conversion
-sed -i "s/^rem /# /gI" *.bat #comments conversion
-sed -i "s/ rem /# /gI" *.bat #comments conversion
-sed -i "s/\"\"/\\\\\"/gI" *.bat #double quotes conversion
+files="*.bat"
+sed -i.bak "s/%\([^%]\+\)%/\$\1/g" $files #Variables conversion
+sed -i "s/if not defined \([^ ]\+\) /[ -z \"\$\1\" ] \&\& /gI" $files
+sed -i "s/if defined \([^ ]\+\) /[ -n \"\$\1\" ] \&\& /gI" $files
+sed -i "s/if not exist \([^ ]\+\) /[ ! -f \$\1 ] \&\& /gI" $files
+sed -i "s/if exist \([^ ]\+\) /[ -f \$\1 ] \&\& /gI" $files
+sed -i "s/if not \([^ ]\+\) /[ ! \1 ] \&\& /gI" $files
+sed -i "s/if \([^ ]\+\) /[ \1 ] \&\& /gI" $files
+sed -i "s/\\\\/\//gI" $files  #path conversion
+sed -i "s/\^|/|/gI" $files #backslashed pipe conversion
+sed -i "s/\brem /# /gI" $files #comments conversion
+sed -i "s/\"\"/\\\\\"/gI" $files #double quotes conversion
+sed -i "s/\bnul\b/\/dev\/null/gI" $files # 'nul' conversion
+sed -i "s/\bset //gI" $files # 'set' conversion
+sed -i "s/\btype /cat /gI" $files # 'type' conversion
+sed -i "s/\bfind /grep /gI" $files # 'find' conversion
